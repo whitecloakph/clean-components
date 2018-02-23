38d987257380be2c18f4ef82440ff37010cc789b
Miguel Fermin <miguel.fermin@whitecloak.com>
2018-02-23 20:48:40 +0800

    Add mvrp activity

---
+++ mvrp-activity/src/main/java/com/whitecloak/mvrpactivity/AutoMvrpActivity.java
@@ -0,0 +1,56 @@
+package com.whitecloak.mvrpactivity;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.whitecloak.cleancomponents.mvp.MvpView;
+import com.whitecloak.cleancomponents.mvrp.MvrpPresenter;
+import com.whitecloak.cleancomponents.mvrp.MvrpRouter;
+
+public abstract class AutoMvrpActivity<V extends MvpView, R extends MvrpRouter, P extends MvrpPresenter<V, R>> extends AppCompatActivity {
+
+    private P mPresenter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupDependencies();
+
+        mPresenter = getMvpPresenter();
+        mPresenter.attachView(getMvpView());
+        mPresenter.attachRouter(getMvpRouter());
+
+        setContentView(getLayoutRes());
+        setupViews();
+
+        loadContent();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPresenter.detachView();
+        mPresenter.detachRouter();
+    }
+
+    @LayoutRes
+    protected abstract int getLayoutRes();
+
+    @NonNull
+    protected abstract V getMvpView();
+
+    @NonNull
+    protected abstract R getMvpRouter();
+
+    @NonNull
+    protected abstract P getMvpPresenter();
+
+    protected abstract void setupDependencies();
+
+    protected abstract void setupViews();
+
+    protected abstract void loadContent();
+}
