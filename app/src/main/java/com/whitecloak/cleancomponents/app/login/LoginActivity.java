85dfd37dce2cc4443df989fffd662350099f8e9a
Miguel Fermin <miguel.fermin@whitecloak.com>
2018-02-25 07:10:09 +0800

    Add login screen

--- app/src/main/java/com/whitecloak/cleancomponents/app/login/LoginActivity.java
+++ app/src/main/java/com/whitecloak/cleancomponents/app/login/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.whitecloak.cleancomponents.app.login;
 
 import android.support.annotation.NonNull;
+import android.widget.TextView;
 
 import com.whitecloak.cleancomponents.R;
 import com.whitecloak.cleancomponents.app.common.base.BaseActivity;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+
 public class LoginActivity extends BaseActivity<LoginView, LoginPresenter> implements LoginView {
+
+    @BindView(R.id.text_username)
+    TextView usernameView;
+
+    @BindView(R.id.text_password)
+    TextView passwordView;
 
     @Inject
     LoginPresenter presenter;
