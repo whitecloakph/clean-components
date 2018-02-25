016d1e44890f3d913305b113477d4a48f9f14b95
Miguel Fermin <miguel.fermin@whitecloak.com>
2018-02-25 19:17:49 +0800

    Add login presenter test

--- app/src/test/java/com/whitecloak/cleancomponents/app/login/LoginPresenterTest.java
+++ app/src/test/java/com/whitecloak/cleancomponents/app/login/LoginPresenterTest.java
@@ -1,11 +1,23 @@
 package com.whitecloak.cleancomponents.app.login;
 
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
-public class LoginPresenterTest {
+import static org.mockito.BDDMockito.then;
+
+public class LoginPresenterTest extends PresenterTest {
+
+    @InjectMocks
+    LoginPresenter presenter;
+
+    @Mock
+    LoginView view;
 
     @Test
-    public void login() throws Exception {
+    public void login_shouldShowMain() throws Exception {
+        presenter.login("username", "password");
 
+        then(view).should().showMain();
     }
 }
---
+++ app/src/test/java/com/whitecloak/cleancomponents/app/login/PresenterTest.java
@@ -0,0 +1,12 @@
+package com.whitecloak.cleancomponents.app.login;
+
+import org.junit.Rule;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+public class PresenterTest {
+
+    @Rule
+    public final MockitoRule mockitoRule = MockitoJUnit.rule();
+}
+
